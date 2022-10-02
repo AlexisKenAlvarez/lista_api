2f98d92e64ba7b06fd4f2fceeb5f5dcf24367510
Alexis Ken Alvarez <alexisken1432@gmail.com>
2022-10-02 10:19:12 +0800

    added promise to sendmail

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -16,13 +16,41 @@ module.exports = async(email, subject, text) => {
 
         })
 
-        await transporter.sendMail({
+        await new Promise((resolve, reject) => {
+            // verify connection configuration
+            transporter.verify(function (error, success) {
+                if (error) {
+                    console.log(error);
+                    reject(error);
+                } else {
+                    console.log("Server is ready to take our messages");
+                    resolve(success);
+                }
+            });
+        });
+
+        const mailData = {
             from: process.env.USER,
             to: email,
             subject: subject,
             text: text
-        })
+        };
+
+        await new Promise((resolve, reject) => {
+            // send mail
+            transporter.sendMail(mailData, (err, info) => {
+                if (err) {
+                    console.error(err);
+                    reject(err);
+                } else {
+                    console.log(info);
+                    resolve(info);
+                }
+            });
+        });
+        
         console.log("Email send sucessful")
+        res.status(200).json({ status: "OK" });
     } catch (error) {
         console.log("Email not sent")
         console.log(error)
